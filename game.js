430d043cf6b8aeef6397fee07ef4235ac58ec684
Bright Uzosike <michizler.brandforyou@gmail.com>
2023-01-01 23:58:19 +0100

    Finished implementing game flow

--- game.js
+++ game.js
@@ -7,6 +7,18 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 // Array to hold user choices from memory
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+
+// keep track if game has started
+$(document).keypress(function() {
+    if (!started) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  });
+
 // listen for user's choices
 $(".btn").click(function() {
 
@@ -20,10 +32,36 @@ $(".btn").click(function() {
     //animate user clicks
     animatePress(userChosenColour);
 
+    checkAnswer(userClickedPattern.length-1);
+
 })
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      setTimeout(function () {
+        $("body").removeClass("game-over");
+      }, 200);
+
+      startOver();
+    }
+}
 
 // handle actions for next sequence to be displayed to user
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
 
     // get a random button color
     var randomNumber = Math.floor(Math.random() * 4);
@@ -52,3 +90,9 @@ function animatePress(currentColor) {
     }, 100);
   }
 
+  // Handles game restart
+  function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
